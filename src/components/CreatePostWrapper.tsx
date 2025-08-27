a029c89ca5de6e502103ea6d446efc5e3e4750c4
Ahmed Mostafa <aldeeb36olymp@gmail.com>
2025-08-27 07:01:01 +0300

    remove all chnages

--- src/components/CreatePostWrapper.tsx
+++
@@ -1,9 +0,0 @@
-'use client';
-import { useUser } from '@clerk/nextjs';
-import CreatePost from './CreatePost';
-
-export default function CreatePostWrapper() {
-  const { isSignedIn } = useUser();
-  if (!isSignedIn) return null;
-  return <CreatePost />;
-}
